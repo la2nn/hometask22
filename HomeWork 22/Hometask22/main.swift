50ab361950b17d4a35caadc86a54dc123354e519
la2nn <nnickspr@gmail.com>
2019-03-31 02:20:24 +0300

    bugs fixed

--- HomeWork 22/Hometask22/main.swift
+++ HomeWork 22/Hometask22/main.swift
@@ -1,10 +1,11 @@
 /* Рассмотрим такую ситуацию: есть семья - папа, мама и дети.
- Папа глава семьи, у него есть Мама, Мама контролирует детей, т.е. иерархия: Папа - Мама - Дети, Дети на одном уровне.
- Дети могут вызывать друг друга и они могут искать пути, как общаться с другими Детьми, например говорить "дай игрушку", спрашивать Маму: "Мама, дай конфетку", общаться с Папой: "Папа, купи игрушку".
- Вся эта иерархия лежит в объекте класса Семья, у которого есть методы, например распечатать всю Семью, е. метод вернёт массив всех членов Семьи.
- У Папы есть 3 кложера (closures) - 1. когда он обращается к Семье - распечатать всю Семью, 2. распечатать Маму, 3. распечатать всех Детей.
- Создать всю иерархию со связями. При выходе из зоны видимости все объекты должны быть уничтожены. */
+Папа глава семьи, у него есть Мама, Мама контролирует детей, т.е. иерархия: Папа - Мама - Дети, Дети на одном уровне.
+Дети могут вызывать друг друга и они могут искать пути, как общаться с другими Детьми, например говорить "дай игрушку", спрашивать Маму: "Мама, дай конфетку", общаться с Папой: "Папа, купи игрушку".
+Вся эта иерархия лежит в объекте класса Семья, у которого есть методы, например распечатать всю Семью, е. метод вернёт массив всех членов Семьи.
+У Папы есть 3 кложера (closures) - 1. когда он обращается к Семье - распечатать всю Семью, 2. распечатать Маму, 3. распечатать всех Детей.
+Создать всю иерархию со связями. При выходе из зоны видимости все объекты должны быть уничтожены. */
 // { [capture list : weak ..? / unowned ... ] in .. }
+
 class Human {
     var name: String
     
@@ -14,40 +15,58 @@ class Human {
 }
 
 class Family {
-    var dad: Father
-    var mom: Mother
+    var dad: Father?
+    var mom: Mother?
     var kids: [Children]
     
     func printFamilyMembers() -> [String] {
         print("Принтим семью ->")
-        var family = [String]()
-        family.append(dad.name)
-        family.append(mom.name)
-        family.append(kids[0].name)
-        family.append(kids[1].name)
-        
-        return family
+        var family = [Human]()
+        if let father = dad { family.append(father) }
+        if let mother = mom { family.append(mother) }
+        kids.forEach { kid in
+            family.append(kid)
+        }
+        return family.map{$0.name}
     }
-    init(dad: Father) {
-        
+    init(dad: Father) {             // если пляшем от батька
         self.dad = dad
-        self.mom = dad.wife
-        self.kids = dad.children!
+        if let wife = dad.wife {
+            self.mom = wife
+        }
+        self.kids = dad.children
+    }
+    
+    init(mom: Mother) {              // если пляшем от детей
+        self.mom = mom
+        if let husband = mom.husband {
+            self.dad = husband
+        }
+        self.kids = mom.children
     }
 }
 
 class Father: Human {
-    var wife: Mother!
-    var children: [Children]!
+    var wife: Mother?
+    var children: [Children] = []
     
     lazy var callFamily: () -> String = { [unowned self] in
-        return "Hey family: \(self.wife.name), \(self.children[0].name), \(self.children[1].name)!" }
-   
+        if let wifeName = self.wife?.name {
+            return "Hey family: \(wifeName), \(self.children[0].name), \(self.children[1].name) !"
+        }
+        return "Hey kids: \(self.children[0].name), \(self.children[1].name) !"
+    }
+    
     lazy var callMother: () -> String = { [unowned self] in
-        return "Hey wife: \(self.wife.name)!" }
+        if let wifeName = self.wife?.name {
+            return "Hey wife: \(wifeName) !"
+        }
+        return "Нема жены"
+    }
     
     lazy var callChildren: () -> String = { [unowned self] in
-        return "Hey kids: \(self.children[0].name), \(self.children[1].name)!" }
+        return "Hey kids: \(self.children[0].name), \(self.children[1].name)!"
+    }
     
     deinit {
         print("Father deinit")
@@ -55,8 +74,8 @@ class Father: Human {
 }
 
 class Mother: Human {
-    weak var husband: Father!
-    var children: [Children]!
+    weak var husband: Father?
+    var children: [Children] = []
     
     deinit {
         print("Mother deinit")
@@ -70,11 +89,17 @@ class Children: Human {
     func giveMeToy() {
         print("\(self.name), let me play with your toy!")
     }
+    
     func giveMeCandy() {
-        print("\(mother!.name), gimme a candy, please!")
+        if let momName = mother?.name {
+           print("\(momName), gimme a candy, please!")
+        }
     }
+    
     func buyToy() {
-        print("\(father!.name), buy me more toys!")
+        if let dadName = father?.name {
+            print("\(dadName), buy me more toys!")
+        }
     }
     
     deinit {
@@ -82,8 +107,8 @@ class Children: Human {
     }
 }
 
-if 5+5 == 10 {
-
+if true {
+    
     let mom = Mother(name: "Маман")
     let dad = Father(name: "Папаc")
     let kid1 = Children(name: "Ребенок1")
@@ -91,9 +116,11 @@ if 5+5 == 10 {
     let kids = [kid1, kid2]
     
     dad.wife = mom
+    //dad.wife = nil       работает тема
     dad.children = kids
     mom.husband = dad
     mom.children = kids
+    
     let family = Family(dad: dad)
     
     print(dad.callFamily())
@@ -101,7 +128,7 @@ if 5+5 == 10 {
     print(dad.callChildren())
     print(family.printFamilyMembers())
     
-    for kid in kids {
+    kids.forEach { kid in
         kid.father = dad
         kid.mother = mom
     }
@@ -112,5 +139,3 @@ if 5+5 == 10 {
     
     print("Минус семья: ")
 }
-
-
