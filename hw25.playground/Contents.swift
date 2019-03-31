9e4bcf78126798a2755a09ec6dee9314b5cb3ebb
la2nn <nnickspr@gmail.com>
2019-03-31 22:41:51 +0300

    fix

--- hw25.playground/Contents.swift
+++ hw25.playground/Contents.swift
@@ -12,19 +12,6 @@ extension UInt8 {
     }
 }
 
-extension UInt64 {
-    func binary() -> String {
-        var result = ""
-        for i in 0..<8 {
-            let mask = 1 << i
-            let set = Int(self) & mask != 0
-            result = (set ? "1" : "0") + result
-        }
-        return result
-    }
-}
-
-
 enum ShoppingList: UInt8 {
     case Bread =    0b00000001
     case Chicken =  0b00000010
@@ -81,17 +68,6 @@ for _ in 0..<16 {
     }
 }
 
-/* var a: UInt8 = 0b00000001
-var b: UInt8 = 0b11111110
-a.binary()
-b.binary()
-a << 2
-(a|b).binary()  // 0 + 0 = 0, иначе 1
-(a&b).binary()  // 1 + 1 = 1, иначе 0
-(a & ~b).binary()
-(a^b).binary()  // 1 + 0 = 1 , иначе 0
-(~a).binary()   // инверсия */
-
 /* 3. Создайте 64х битное число, которое представляет клетки на шахматной доске. Установите биты так, что 0 - это белое поле, а 1 - черное. Младший бит это клетка а1 и каждый следующий байт начинается с клетки а (а2, а3, а4) и заканчивается клеткой h(h2, h3, h4). Выбирая клетки но индексу столбца и строки определите цвет клетки опираясь исключительно на значение соответствующего бита */
 
 let chessboard: UInt64 = 0b10101010_01010101_10101010_01010101_10101010_01010101_10101010_01010101
@@ -111,5 +87,5 @@ func cellColor(_ column: Column, _ row: UInt64) {
 
 cellColor(.A, 1)        // white
 cellColor(.A, 2)        // white
-cellColor(.G, 8)        // whie
+cellColor(.G, 8)        // white
 
