81b860ac4ebb787f3e5500d4d598ce0898ec636c
la2nn <nnickspr@gmail.com>
2019-05-14 23:00:10 +0300

    HW14NSString

---
+++ ObjC/HW14Strings/HW14Strings/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HW14Strings
+//
+//  Created by Nick on 14.05.2019.
+//  Copyright © 2019 nick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
